90a305d5feff405d6991ad55f27ee204066d8df6
Luan Dantas <lds16@aluno.ifal.edu.br>
2025-08-14 13:35:11 -0300

    Criando os elementos básicos

--- main.py
+++ main.py
@@ -27,4 +27,22 @@ janela.title("Cronômetro Pomodoro")
 janela.geometry("400x250")
 janela.resizable(False, False)
 
+rotulo_timer = tk.Label(janela, text="25:00", font=("Helvetica", 48))
+rotulo_timer.pack(pady=20)
+
+rotulo_estado = tk.Label(janela, text="Estado: Pomodoro", font=("Helvetica", 14))
+rotulo_estado.pack()
+
+rotulo_pomodoros = tk.Label(janela, text="Pomodotos completos: 0", font=("Helvetica", 14))
+rotulo_pomodoros.pack()
+
+frame_botoes = tk.Frame(janela)
+frame_botoes.pack(pady=10)
+
+botao_iniciar_pausar = tk.Button(frame_botoes, text="Iniciar", command=iniciar_pausar)
+botao_iniciar_pausar.pack(side=tk.LEFT, padx=5)
+
+botao_resetar = tk.Button(frame_botoes, text="Resetar", command=resetar)
+botao_resetar.pack(side=tk.LEFT, padx=5)
+
 janela.mainloop()
