ffd90dec529811a28ae46b2be77036165c54c089
Luan Dantas <lds16@aluno.ifal.edu.br>
2025-08-14 13:21:48 -0300

    criando arquivo princiapla

---
+++ mian.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from tkinter import messagebox
+import time
+from plyer import notification
+
+
+pomodoro_tempo = 25 * 60  
+descanso_curto_tempo = 5 * 60  
+descanso_longo_tempo = 15 * 60 
+tempo_restante = pomodoro_tempo
+executando = False
+pomodoros_completos = 0
+estado_atual = "Pomodoro"
+
+def atualizar_tempo():
+    pass 
+
+def iniciar_pausar():
+    pass 
+
+def resetar():
+    pass
